5cf44224f351a72f2b20de6299c01d1f360b6d55
ZachYlst <ZachYlst@users.noreply.github.com>
2017-10-23 09:54:02 -0600

    Created repository for Chatbot project.

---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,13 @@
+package chat.controller;
+
+import chat.view.PopupDisplay;
+
+public class ChatRunner
+{
+	public static void main(String [] args)
+	{
+		PopupDisplay sample = new PopupDisplay();
+		sample.displayText("got 2 go fast");
+		sample.collectResponse("do you got 2 go fast?");
+	}
+}
